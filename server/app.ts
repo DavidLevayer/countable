eb37438b282a15cb7ae939256840c9926974f59f
DavidLevayer <david.levayer@gmail.com>
2016-12-26 21:19:49 +0100

    Simple server implementation

---
+++ server/app.ts
@@ -0,0 +1,40 @@
+import * as express from 'express';
+import { json, urlencoded } from 'body-parser';
+import * as path from 'path';
+
+import { pingRouter } from './routes/ping/ping.router';
+
+const app: express.Application = express();
+
+app.disable('x-powered-by');
+
+app.use(json());
+app.use(urlencoded({ extended: false }));
+
+// api routes
+app.use('/api/v1', pingRouter);
+
+if (app.get('env') === 'production') {
+
+  // in production mode run application from dist folder
+  app.use(express.static(path.join(__dirname, '/../client')));
+}
+
+// catch 404 and forward to error handler
+app.use(function (req: express.Request, res: express.Response, next) {
+  let err = new Error('Not Found');
+  next(err);
+});
+
+// production error handler
+// no stacktrace leaked to user
+app.use(function (err: any, req: express.Request, res: express.Response, next: express.NextFunction) {
+
+  res.status(err.status || 500);
+  res.json({
+    error: {},
+    message: err.message
+  });
+});
+
+export { app }
---
+++ server/routes/ping/ping.router.ts
@@ -0,0 +1,35 @@
+import { Router, Request, Response, NextFunction } from 'express';
+
+class PingRouter {
+
+  router: Router;
+
+  /**
+   * Initialize the PingRouter
+   */
+  constructor() {
+    this.router = Router();
+    this.init();
+  }
+
+  /**
+   * GET all Heroes.
+   */
+  public getApiStatus(req: Request, res: Response, next: NextFunction) {
+    res.json({
+      message: 'Api working!'
+    });
+  }
+
+  /**
+   * Take each handler, and attach to one of the Express.Router's
+   * endpoints.
+   */
+  init() {
+    this.router.get('/', this.getApiStatus);
+  }
+
+}
+
+// Export configured router
+export var pingRouter = new PingRouter().router;
