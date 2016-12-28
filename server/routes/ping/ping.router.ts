6f462279dd699367bb95d50e2ee44147ad6a32c4
DavidLevayer <david.levayer@gmail.com>
2016-12-28 19:09:41 +0100

    Updates ping router

--- server/routes/ping/ping.router.ts
+++ server/routes/ping/ping.router.ts
@@ -1,36 +1,10 @@
-import { Router, Request, Response, NextFunction } from 'express';
+import { Controller, Get } from "ts-express-decorators";
 
-class PingRouter {
+@Controller('/')
+export class PingRouter {
 
-  router: Router;
-
-  /**
-   * Initialize the PingRouter
-   */
-  constructor() {
-    this.router = Router();
-    this.init();
+  @Get('')
+  public get() {
+    return { message: 'Api working!' };
   }
-
-  /**
-   * GET all Heroes.
-   */
-  public getApiStatus(req: Request, res: Response, next: NextFunction) {
-    res.json({
-      message: 'Api working!'
-    });
-  }
-
-  /**
-   * Take each handler, and attach to one of the Express.Router's
-   * endpoints.
-   */
-  init() {
-    this.router.get('/', this.getApiStatus);
-  }
-
 }
-
-// Export configured router
-let pingRouter = new PingRouter().router;
-export { pingRouter }
