cc52bed190ec8d9aa5e4050c0ce572146d5dd4eb
DavidLevayer <david.levayer@gmail.com>
2016-12-29 13:24:00 +0100

    Creates database service

---
+++ server/services/database/database.service.ts
@@ -0,0 +1,23 @@
+import { Service } from 'ts-express-decorators';
+import { IDatabaseConnector } from './database.connector';
+import { SQLiteConnector } from './sqlite.connector';
+import { populationQueries } from './population-queries';
+
+@Service()
+export class DatabaseService {
+
+  private databaseConnector: IDatabaseConnector;
+
+  constructor() {
+
+    let db = new SQLiteConnector('./countable-database.sqlite', true);
+    populationQueries.forEach(function (query) {
+      db.executeQuery(query, []);
+    });
+    this.databaseConnector = db;
+  }
+
+  public executeQuery(query: string, ...params: any[]): Promise<any[]> {
+    return this.databaseConnector.executeQuery(query, params);
+  }
+}
