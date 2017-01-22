db364ac4f61d63e162f21db21478fbd8e6451dad
DavidLevayer <david.levayer@gmail.com>
2017-01-22 15:20:50 +0100

    Add better types and improve log process

--- server/routes/account/account.router.ts
+++ server/routes/account/account.router.ts
@@ -1,6 +1,7 @@
 import { Controller, Get } from 'ts-express-decorators';
 import { AccountService } from '../../services/account/account.service';
 import { BasicRouter } from '../basic.router';
+import { Request, Response } from 'express';
 
 @Controller('/account')
 export class AccountRouter extends BasicRouter {
@@ -10,17 +11,18 @@ export class AccountRouter extends BasicRouter {
   }
 
   @Get('/')
-  public getAll(req, res) {
+  public getAll(req: Request, res: Response) {
 
     return this.accountService.getAll().then((rows) => {
       res.json(rows);
     }).catch(err => {
+      this.logError(err);
       this.throwError(res, 'An error has occured while getting accounts');
     });
   }
 
   @Get('/:id')
-  public get(req, res) {
+  public get(req: Request, res: Response) {
 
     let id: number = +req.params.id;
 
@@ -30,6 +32,7 @@ export class AccountRouter extends BasicRouter {
       return this.accountService.get(id).then((rows) => {
         res.json(rows);
       }).catch(err => {
+        this.logError(err);
         this.throwError(res, 'An error has occured while getting account ' + id);
       });
     }
--- server/routes/basic.router.ts
+++ server/routes/basic.router.ts
@@ -3,18 +3,24 @@ import { CrudRouter } from './crud.router';
 export class BasicRouter implements CrudRouter {
 
   /**
-   * Log the error message, set status and returns a JSON object containing the error message
+   * Set status and returns a JSON object containing the error message
    * @param res
    * @param message
    * @param status
    */
   public throwError(res, message: string, status: number = 500): void {
-    // Log error
-    console.error('Error ' + status + ': ' + message);
     // Set response status and message
     res.status(status);
     res.json({
       message: message
     });
+  }
+
+  /**
+   * Log the given message as an error
+   * @param message
+   */
+  public logError(message: string): void {
+    console.error(message);
   }
 }
