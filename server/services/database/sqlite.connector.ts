c9c86ce0a8b486001ed4487f2b8c3532e10c7a24
David Levayer <david.levayer@gmail.com>
2017-02-05 20:42:46 +0100

    Refactor connector: remove statement step

--- server/services/database/sqlite.connector.ts
+++ server/services/database/sqlite.connector.ts
@@ -1,4 +1,4 @@
-import { Database, Statement } from 'sqlite3';
+import { Database } from 'sqlite3';
 import { IDatabaseConnector } from './database.connector';
 
 /* tslint:disable:no-console */
@@ -25,52 +25,14 @@ export class SQLiteConnector implements IDatabaseConnector {
    */
   public executeQuery(query: string, ...params: any[]): Promise<any[]> {
 
-    let promise: Promise<Statement> = this.prepare(query);
-
     if (this.debug) {
       console.info('\nRunning query: ' + query);
       console.info('With params: ' + params);
     }
 
-    return promise.then((statement: Statement) => {
-      return this.all(statement, params);
-    });
-  }
-
-  /**
-   * Prepares the SQL statement
-   * @param query the query to prepare
-   * @returns {Promise<Statement>} the query Statement object
-   */
-  private prepare(query: string): Promise<Statement> {
     return new Promise((resolve, reject) => {
-      const statement = this.db.prepare(query, (err) => {
+      this.db.all(query, params, function (err, rows) {
         if (err) {
-          if (this.debug) {
-            console.info('Error: cannot prepare query: ' + err);
-          }
-          reject(err);
-        } else {
-          resolve(statement);
-        }
-      });
-    });
-  }
-
-  /**
-   * Runs the SQL query with the specified parameters and returns all result rows afterwards.
-   * @param statement
-   * @param params
-   * @returns {Promise<T>}
-   */
-  private all(statement: Statement, params?: any[]): Promise<any[]> {
-
-    return new Promise((resolve, reject) => {
-      statement.all(params, function (err, rows) {
-        if (err) {
-          if (this.debug) {
-            console.info('Error: cannot execute query: ' + err);
-          }
           reject(err);
         } else {
           resolve(rows);
