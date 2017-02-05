5c4097ce65917bc07cfd9a4bad33f75a139ff661
David Levayer <david.levayer@gmail.com>
2017-02-05 21:56:47 +0100

    Update database service mock

--- server/services/database/database.service.mock.ts
+++ server/services/database/database.service.mock.ts
@@ -1,11 +1,18 @@
-
 export class DatabaseServiceMock {
 
   public toReturn: any;
   public shouldFail = false;
   public failMessage: string;
 
-  public executeQuery(query: string, ...params: any[]): Promise<any[]> {
+  public select(query: string, ...params: any[]) {
+    return this.mockQuery();
+  }
+
+  public insert(query: string, ...params: any[]) {
+    return this.mockQuery();
+  }
+
+  private mockQuery(): Promise<any[]> {
     if (this.shouldFail) {
       return Promise.reject(this.failMessage);
     }
