6f8a9e1a4190a5f2cc1801b40daf441ddc98b0ac
David Levayer <david.levayer@gmail.com>
2017-02-05 18:28:57 +0100

    Make some field 'UNIQUE'

--- server/services/database/population-queries.ts
+++ server/services/database/population-queries.ts
@@ -1,17 +1,17 @@
 const accountTableQuery: string =
   'CREATE TABLE IF NOT EXISTS Account(' +
   'accountId INTEGER PRIMARY KEY,' +
-  'name TEXT NOT NULL);';
+  'name TEXT NOT NULL UNIQUE);';
 
 const categoryTableQuery: string =
   'CREATE TABLE IF NOT EXISTS Category(' +
   'categoryId INTEGER PRIMARY KEY,' +
-  'name TEXT NOT NULL);';
+  'name TEXT NOT NULL UNIQUE);';
 
 const subcategoryTableQuery: string =
   'CREATE TABLE IF NOT EXISTS Subcategory(' +
   'subcategoryId INTEGER PRIMARY KEY,' +
-  'name TEXT NOT NULL,' +
+  'name TEXT NOT NULL UNIQUE,' +
   'refCategory INTEGER NOT NULL,' +
   'FOREIGN KEY(refCategory) REFERENCES Category(categoryId));';
 
