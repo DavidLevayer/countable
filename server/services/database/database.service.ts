061f2efde8d3b81fbf4d5d419627b83275f0d580
David Levayer <david.levayer@gmail.com>
2017-02-05 21:18:31 +0100

    Add comment and update signature type

--- server/services/database/database.service.ts
+++ server/services/database/database.service.ts
@@ -17,19 +17,43 @@ export class DatabaseService {
     this.databaseConnector = db;
   }
 
+  /**
+   * Executes query with given params and returns rows
+   * @param query
+   * @param params
+   * @returns {Promise<any[]>}
+   */
   public select(query: string, ...params: any[]): Promise<any[]> {
     return this.databaseConnector.executeQuery(QueryType.SELECT, query, ...params);
   }
 
-  public insert(query: string, ...params: any[]): Promise<any[]> {
+  /**
+   * Executes query with given params and returns the unique id of the inserted element
+   * @param query
+   * @param params
+   * @returns {Promise<any[]>}
+   */
+  public insert(query: string, ...params: any[]): Promise<number> {
     return this.databaseConnector.executeQuery(QueryType.INSERT, query, ...params);
   }
 
-  public update(query: string, ...params: any[]): Promise<any[]> {
+  /**
+   * Executes query with given params and returns the number of updated rows
+   * @param query
+   * @param params
+   * @returns {Promise<any[]>}
+   */
+  public update(query: string, ...params: any[]): Promise<number> {
     return this.databaseConnector.executeQuery(QueryType.UPDATE, query, ...params);
   }
 
-  public delete(query: string, ...params: any[]): Promise<any[]> {
+  /**
+   * Executes query with given params and returns the number of deleted rows
+   * @param query
+   * @param params
+   * @returns {Promise<any[]>}
+   */
+  public delete(query: string, ...params: any[]): Promise<number> {
     return this.databaseConnector.executeQuery(QueryType.DELETE, query, ...params);
   }
 }
