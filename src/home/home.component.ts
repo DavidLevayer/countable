e10752bf044b2ea7b4baa6a5ae29612d67233bab
David Levayer <david.levayer@gmail.com>
2017-04-22 18:14:20 +0200

    Initiate dashboard

--- src/home/home.component.ts
+++ src/home/home.component.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
+import { Account } from '../shared/model/account';
+import { Category } from '../shared/model/category';
 
 @Component({
   selector: 'app-home',
-  template: '<h1>Home</h1>'
+  templateUrl: 'home.component.html',
+
+  styleUrls: [
+    '../shared/scss/highlight.common.scss',
+    '../shared/scss/callout.common.scss'
+  ]
 })
 export class HomeComponent {
+
+  /** List of accounts */
+  accounts: Account[] = [];
+  /** List of categories */
+  categories: Category[] = [];
+  /** Contains an eventual error */
+  error: Error;
 }
